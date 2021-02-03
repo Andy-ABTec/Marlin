de863fa8d18aac0fcf3b9c7e4ceaf58249015594
Andy Barnett <andy.abtec@gmail.com>
2021-02-03 07:37:48 +0000

    added uuid

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -134,11 +134,11 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender-3"
+#define CUSTOM_MACHINE_NAME "Ender-3+BLTouch"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
-//#define MACHINE_UUID "00000000-0000-0000-0000-000000000000"
+#define MACHINE_UUID "31963356-fd60-4aa1-b0c8-deee061b0c39"
 
 // @section extruder
 
